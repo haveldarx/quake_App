d0475a97d888c348cdee1dc8c675f9da72941577
haveldarx <pavan.gangadhar.2001@gmail.com>
2021-07-17 20:43:37 +0530

    added map

--- qapp/lib/main.dart
+++ qapp/lib/main.dart
@@ -2,37 +2,37 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
+// ignore: unused_element
+Map _data = ( getQuakes()) as Map ;
 void main() async{
-  List _quakes = await getQuakes();
-  print(_quakes );
+   _data = (await getQuakes()) as Map;
+  print(_data['features'][0]['properties']);
   runApp(new MaterialApp(
-    home: quakes(),
+    home: new Quakes(), 
   ));
-}
-class quakes extends StatelessWidget {
-  const quakes({ Key? key }) : super(key: key);
-
-  get http => null;
-
+  }
+  class Quakes extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title:Text( 'QuakeHome',
-        style: TextStyle(color: Colors.cyan),
-        
-        ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title:  new Text('Quakes'),
         centerTitle: true,
-        backgroundColor: Colors.white70,
+      
       ),
-      body: Container(
-        
-      ),
+      body:new Center(
+        child:  new ListView.builder(itemBuilder:(BuildContext context,int postition) {
+          return new ListTile(
+             title:  new Text("$_data['features']['postiton']['properties']['place']"),
+          );
+        },
+        itemCount:_data.length ,),
+      ) ,
     );
   }
-}
-    Future <List> getQuakes() async {
+
+  }
+Future <List> getQuakes() async {
      Uri apiUrl = Uri.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
     
   
